ccc63a64956c92e0d567cba297508a7fc4da14a3
Oskar Meszaros <aspen_davisGAED@protonmail.com>
2025-04-03 17:55:32 +1100

    feat: fixed smoothing

--- Embedded Systems/Coding/smoothed_pot_control/smoothed_pot_control.ino
+++ Embedded Systems/Coding/smoothed_pot_control/smoothed_pot_control.ino
@@ -3,10 +3,10 @@
 /* Define I/O pins */
 const int POT1 = A0;
 const int POT2 = A1;
-const int LED5 = 2; // LED1, LED3 in use
-const int LED6 = 3; // LED2, LED4 in use
-const int SWITCH1 = 4;
-const int SWITCH2 = 5;
+const int LEDPIN1 = 2; // LED1 in use
+const int LEDPIN2 = 3; // LED2 in use
+const int SWITCH2 = 4;
+const int SWITCH1 = 5;
 const int SERVO1 = 6;
 const int SERVO2 = 7;
 
@@ -19,10 +19,15 @@ int servo1_pos = 0;
 int servo2_pos = 0;
 int pot1_value = 0;
 int pot2_value = 0;
+int pot1_old = 0;
+int pot2_old = 0;
 int switch1_value = LOW; // LOW = Smoothing off, HIGH = Smoothing on
 int switch2_value = LOW;
 
-const int smoothingSteps = 16;
+int threshold = 4;
+bool moving = false;
+
+const int smoothingSteps = 48;
 int pot1_readings[smoothingSteps] = {0};
 int pot2_readings[smoothingSteps] = {0};
 int pot1_running_total = 0;
@@ -35,25 +40,33 @@ int pot_to_servo(int value) {
 
 int smooth_value(int value, int readings[]) {
   /* Remove the oldest reading, append new reading, and return average */
-  int old = readings[0];
   int total = 0;
   
-  for (int i = 0; i < smoothingSteps-1, i++;) {
+  for (int i = 0; i < smoothingSteps-1; i++) {
     readings[i] = readings[i+1];
-  }
-  readings[smoothingSteps-1] = value;
-
-  for (int i = 0; i < smoothingSteps, i++;) {
     total += readings[i];
   }
+
+  readings[smoothingSteps-1] = value;
+  total += value;
   
   return (int)(total/smoothingSteps);
 }
 
+void calibrate(int cycles) {
+  /*
+  Propogates the buffer with values to prevent the servo moving prior to 
+  */
+  for (int i = 0; i < cycles; i++){
+    pot1_value = analogRead(POT1);
+    pot2_value = analogRead(POT2);
+  }
+}
+
 void setup() {
   /* Configure pins*/
-  pinMode(LED5, OUTPUT);
-  pinMode(LED6, OUTPUT);
+  pinMode(LEDPIN1, OUTPUT);
+  pinMode(LEDPIN2, OUTPUT);
   pinMode(SWITCH1, INPUT);
   pinMode(SWITCH2, INPUT);
 
@@ -66,6 +79,8 @@ void setup() {
 }
 
 void loop() {
+  pot1_old = pot1_value;
+  pot2_old = pot2_value;
   pot1_value = analogRead(POT1);
   pot2_value = analogRead(POT2);
 
@@ -79,21 +94,32 @@ void loop() {
     pot2_value = smooth_value(pot2_value, pot2_readings);
   }
 
+  int delta1 = pot1_old - pot1_value;
+  int delta2 = pot2_old - pot2_value;
+
+  if ((delta1 < -threshold) or (delta1 > threshold)) {
+    moving = true;
+  }
+  if ((delta2 < -threshold) or (delta1 > threshold)) {
+    moving = true;
+  }
+
+  if (moving) {
+    moving = false;
+    digitalWrite(LEDPIN1, HIGH);
+  } else {
+    digitalWrite(LEDPIN1, LOW);
+  }
+
   servo1_pos = pot_to_servo(pot1_value);
   servo2_pos = pot_to_servo(pot2_value);
 
   servo1.write(servo1_pos);
   servo2.write(servo2_pos);
 
-  for (int i = 0; i < smoothingSteps; i++) {
-    Serial.print(pot1_readings[i]);
-    Serial.print("\t");
-  }
-  Serial.println();
-
-  // Serial.println(pot1_readings);
-  // Serial.print("\t");
-  // Serial.println(pot2_value);
+  Serial.print(pot1_value);
+  Serial.print("\t");
+  Serial.println(pot2_value);
 
   delay(10);
 }
